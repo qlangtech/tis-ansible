db54f625eadc616f62c9f22689fdabbb32f3d806
baisui <baisui@2dfire.com>
2019-04-21 22:18:05 +0800

    add comment

--- tis_console.sql
+++ tis_console.sql
@@ -23,15 +23,15 @@ DROP TABLE IF EXISTS `app_trigger_job_relation`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `app_trigger_job_relation` (
-  `at_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `app_id` bigint(20) NOT NULL,
-  `job_type` tinyint(4) NOT NULL,
-  `job_id` bigint(20) NOT NULL,
-  `crontab` varchar(20) NOT NULL,
-  `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `gmt_modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `project_name` varchar(40) NOT NULL,
-  `is_stop` char(1) NOT NULL DEFAULT 'Y',
+  `at_id` bigint(20) NOT NULL AUTO_INCREMENT comment 'at_id',
+  `app_id` bigint(20) NOT NULL comment 'app_id',
+  `job_type` tinyint(4) NOT NULL comment 'job_type',
+  `job_id` bigint(20) NOT NULL comment 'job_id',
+  `crontab` varchar(20) NOT NULL comment 'crontab',
+  `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment 'gmt_create',
+  `gmt_modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' comment 'gmt_modified',
+  `project_name` varchar(40) NOT NULL comment 'project_name',
+  `is_stop` char(1) NOT NULL DEFAULT 'Y' comment 'is_stop',
   PRIMARY KEY (`at_id`),
   KEY `application_idx_app_id` (`app_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
@@ -54,7 +54,7 @@ CREATE TABLE `application` (
   `is_auto_deploy` char(1) NOT NULL DEFAULT 'N' COMMENT '自动部署',
   `dpt_id` int(11) NOT NULL COMMENT 'dpt_id',
   `dpt_name` varchar(50) DEFAULT NULL COMMENT 'dpt_name',
-  `full_build_cron_time` varchar(50) DEFAULT '',
+  `full_build_cron_time` varchar(50) DEFAULT 'full_build_cron_time',
   PRIMARY KEY (`app_id`),
   UNIQUE KEY `application_idx_projname_delete` (`project_name`),
   KEY `fk_ref2` (`dpt_id`)
@@ -71,11 +71,11 @@ CREATE TABLE `application` (
 
 DROP TABLE IF EXISTS `cluster_snapshot`;
 CREATE TABLE `cluster_snapshot` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `data_type` varchar(20) NOT NULL,
-  `incr_number` int(11) NOT NULL,
-  `app_id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT comment 'id',
+  `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP comment 'gmt_create',
+  `data_type` varchar(20) NOT NULL comment 'data_type',
+  `incr_number` int(11) NOT NULL comment 'incr_number',
+  `app_id` bigint(20) NOT NULL comment 'app_id',
   PRIMARY KEY (`id`),
   UNIQUE KEY `cluster_snapshot_idx_projname_delete` (`app_id`,`data_type`,`gmt_create`)
 ) ENGINE=MEMORY  DEFAULT CHARSET=utf8 MAX_ROWS=100000000;
@@ -89,11 +89,11 @@ DROP TABLE IF EXISTS `cluster_snapshot_pre_day`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cluster_snapshot_pre_day` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `gmt_create` date NOT NULL,
-  `data_type` varchar(20) NOT NULL,
-  `incr_number` int(11) NOT NULL,
-  `app_id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT comment 'id',
+  `gmt_create` date NOT NULL comment 'gmt_create',
+  `data_type` varchar(20) NOT NULL comment 'data_type',
+  `incr_number` int(11) NOT NULL comment 'incr_number',
+  `app_id` bigint(20) NOT NULL comment 'app_id',
   PRIMARY KEY (`id`),
   UNIQUE KEY `cluster_snapshot_pre_day_idx_projname_delete` (`app_id`,`data_type`,`gmt_create`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
@@ -109,7 +109,7 @@ DROP TABLE IF EXISTS `department`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `department` (
-  `dpt_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'dpt_id',
+  `dpt_id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT 'dpt_id',
   `parent_id` int(11) DEFAULT NULL COMMENT 'parent_id',
   `name` varchar(30) NOT NULL COMMENT 'name',
   `gmt_create` datetime NOT NULL COMMENT 'gmt_create',
@@ -139,7 +139,7 @@ DROP TABLE IF EXISTS `nums`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `nums` (
-  `a` int(11) NOT NULL
+  `a` int(11) NOT NULL commnet 'a'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -151,7 +151,7 @@ DROP TABLE IF EXISTS `operation_log`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `operation_log` (
-  `op_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'op_id',
+  `op_id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT 'op_id',
   `usr_id` varchar(30) NOT NULL COMMENT 'usr_id',
   `usr_name` varchar(30) NOT NULL COMMENT 'usr_name',
   `op_type` varchar(30) NOT NULL COMMENT 'op_type',
@@ -183,7 +183,7 @@ CREATE TABLE `resource_parameters` (
   `daily_value` varchar(100) DEFAULT NULL COMMENT '...',
   `ready_value` varchar(100) DEFAULT NULL COMMENT '...',
   `online_value` varchar(100) DEFAULT NULL COMMENT '...',
-  `desc` varchar(200) DEFAULT NULL COMMENT 'key 描述',
+  `param_desc` varchar(200) DEFAULT NULL COMMENT 'param_desc 描述',
   `gmt_create` datetime NOT NULL COMMENT '...',
   `gmt_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '...',
   PRIMARY KEY (`rp_id`),
@@ -199,7 +199,7 @@ DROP TABLE IF EXISTS `server_group`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `server_group` (
-  `gid` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `gid` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `app_id` int(11) NOT NULL COMMENT 'appid',
   `runt_environment` smallint(6) NOT NULL COMMENT '针对应用类型：0：日常 1 daily 2：线上',
   `group_index` smallint(6) NOT NULL COMMENT 'group_index',
@@ -221,7 +221,7 @@ DROP TABLE IF EXISTS `snapshot`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `snapshot` (
-  `sn_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'sn_id',
+  `sn_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'sn_id',
   `create_time` datetime NOT NULL COMMENT 'create_time',
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'update_time',
   `app_id` int(11) NOT NULL COMMENT 'app_id',
@@ -242,14 +242,6 @@ CREATE TABLE `snapshot` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COMMENT='记录当前发布的记录，对以下记录改动之后都会新添加一条记录1 配置文件修改2 代码包上传之后的';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `t_service`
---
-
-
---
--- Table structure for table `t_user_config`
---
 
 
 --
@@ -260,14 +252,14 @@ DROP TABLE IF EXISTS `table_dump`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `table_dump` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `datasource_table_id` int(11) NOT NULL,
-  `hive_table_name` varchar(50) DEFAULT NULL,
-  `state` tinyint(1) DEFAULT NULL COMMENT '',
-  `info` text COMMENT '',
-  `is_valid` tinyint(1) NOT NULL DEFAULT '1',
-  `create_time` datetime DEFAULT NULL,
-  `op_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT comment 'id',
+  `datasource_table_id` int(11) NOT NULL comment 'datasource_table_id',
+  `hive_table_name` varchar(50) DEFAULT NULL comment 'hive_table_name',
+  `state` tinyint(1) DEFAULT NULL COMMENT 'state',
+  `info` text COMMENT 'info',
+  `is_valid` tinyint(1) NOT NULL DEFAULT '1' comment 'is_valid',
+  `create_time` datetime DEFAULT NULL comment 'create_time',
+  `op_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment 'op_time',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_table_dump_hive_table_name` (`hive_table_name`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
@@ -282,13 +274,13 @@ DROP TABLE IF EXISTS `trigger_job`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `trigger_job` (
-  `job_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `domain` varchar(10) DEFAULT NULL,
-  `crontab` varchar(30) NOT NULL,
-  `gmt_create` datetime NOT NULL,
-  `gmt_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `is_stop` char(1) NOT NULL DEFAULT 'Y',
-  `is_stop_ready` char(1) NOT NULL DEFAULT 'Y',
+  `job_id` bigint(20) NOT NULL AUTO_INCREMENT comment 'job_id',
+  `domain` varchar(10) DEFAULT NULL comment 'domain',
+  `crontab` varchar(30) NOT NULL comment 'crontab',
+  `gmt_create` datetime NOT NULL comment 'gmt_create',
+  `gmt_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment 'gmt_modified',
+  `is_stop` char(1) NOT NULL DEFAULT 'Y' comment 'is_stop',
+  `is_stop_ready` char(1) NOT NULL DEFAULT 'Y' comment 'is_stop_ready',
   PRIMARY KEY (`job_id`),
   KEY `idx_job_id` (`job_id`),
   KEY `idx_domain_gmt` (`domain`,`gmt_modified`)
@@ -323,17 +315,17 @@ DROP TABLE IF EXISTS `usr_dpt_relation`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usr_dpt_relation` (
-  `usr_id` varchar(40) NOT NULL,
-  `dpt_id` bigint(20) NOT NULL,
-  `dpt_name` varchar(100) NOT NULL,
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `user_name` varchar(100) NOT NULL,
-  `real_name` varchar(32) DEFAULT NULL,
-  `pass_word` varchar(32) DEFAULT NULL,
-  `r_id` bigint(20) NOT NULL,
-  `role_name` varchar(20) DEFAULT NULL,
-  `extra_dpt_relation` char(1) NOT NULL DEFAULT 'N',
+  `usr_id` varchar(40) NOT NULL comment 'usr_id',
+  `dpt_id` bigint(20) NOT NULL comment 'dpt_id',
+  `dpt_name` varchar(100) NOT NULL comment 'dpt_name',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment 'create_time',
+  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' comment 'update_time',
+  `user_name` varchar(100) NOT NULL comment 'user_name',
+  `real_name` varchar(32) DEFAULT NULL comment 'real_name',
+  `pass_word` varchar(32) DEFAULT NULL comment 'password',
+  `r_id` bigint(20) NOT NULL comment 'r_id',
+  `role_name` varchar(20) DEFAULT NULL comment 'role_name',
+  `extra_dpt_relation` char(1) NOT NULL DEFAULT 'N' comment 'extra_dpt_relation' ,
   PRIMARY KEY (`usr_id`),
   UNIQUE KEY `idx_user_name_usr_dpt_relation` (`user_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -351,14 +343,14 @@ DROP TABLE IF EXISTS `work_flow`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `work_flow` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `op_user_id` int(11) NOT NULL,
-  `op_user_name` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
-  `git_path` varchar(50) NOT NULL,
-  `in_change` tinyint(4) NOT NULL DEFAULT '0',
-  `create_time` datetime DEFAULT NULL,
-  `op_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` int(11) NOT NULL AUTO_INCREMENT comment 'id',
+  `name` varchar(50) DEFAULT NULL comment 'name',
+  `op_user_id` int(11) NOT NULL comment 'op_user_id',
+  `op_user_name` varchar(50) CHARACTER SET utf8 DEFAULT NULL comment 'op_user_name',
+  `git_path` varchar(50) NOT NULL comment 'git_path',
+  `in_change` tinyint(4) NOT NULL DEFAULT '0'  comment 'in_change',
+  `create_time` datetime DEFAULT NULL comment 'create_time',
+  `op_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment 'op_time',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 ;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -371,21 +363,21 @@ DROP TABLE IF EXISTS `work_flow_build_history`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `work_flow_build_history` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `start_time` datetime DEFAULT NULL,
-  `end_time` datetime DEFAULT NULL,
-  `state` tinyint(4) DEFAULT NULL ,
-  `trigger_type` tinyint(1) DEFAULT NULL ,
-  `op_user_id` int(11) DEFAULT NULL,
-  `op_user_name` varchar(50) DEFAULT NULL,
-  `app_id` int(11) DEFAULT NULL,
-  `app_name` varchar(40) DEFAULT NULL,
+  `id` bigint(11) NOT NULL AUTO_INCREMENT comment 'id',
+  `start_time` datetime DEFAULT NULL comment 'start_time',
+  `end_time` datetime DEFAULT NULL comment 'end_time',
+  `state` tinyint(4) DEFAULT NULL comment 'state',
+  `trigger_type` tinyint(1) DEFAULT NULL comment 'trigger_type',
+  `op_user_id` int(11) DEFAULT NULL comment 'op_user_id',
+  `op_user_name` varchar(50) DEFAULT NULL comment 'op_user_name',
+  `app_id` int(11) DEFAULT NULL comment 'app_id',
+  `app_name` varchar(40) DEFAULT NULL comment 'app_name',
   `start_phase` tinyint(4) DEFAULT NULL COMMENT '1:dump 2:join 3 build 4 backflow ',
-  `history_id` int(11) DEFAULT NULL,
-  `work_flow_id` int(11) DEFAULT NULL,
-  `create_time` datetime DEFAULT NULL,
-  `op_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `end_phase` tinyint(4) DEFAULT NULL,
+  `history_id` int(11) DEFAULT NULL comment 'history_id',
+  `work_flow_id` int(11) DEFAULT NULL comment 'work_flow_id',
+  `create_time` datetime DEFAULT NULL comment 'create_time',
+  `op_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment 'op_time',
+  `end_phase` tinyint(4) DEFAULT NULL comment 'end_phase',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -398,13 +390,13 @@ DROP TABLE IF EXISTS `work_flow_build_phase`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `work_flow_build_phase` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `work_flow_build_history_id` int(11) DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT comment 'id',
+  `work_flow_build_history_id` int(11) DEFAULT NULL comment 'work_flow_build_history_id' ,
   `phase` tinyint(4) DEFAULT NULL COMMENT 'phase index',
-  `result` tinyint(1) DEFAULT NULL,
-  `phase_info` text COMMENT 'phase status',
-  `create_time` datetime DEFAULT NULL,
-  `op_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `result` tinyint(1) DEFAULT NULL comment 'result',
+  `phase_info` text COMMENT 'phase status' comment 'phase_info',
+  `create_time` datetime DEFAULT NULL comment 'create_time',
+  `op_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment 'op_time',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -417,18 +409,18 @@ DROP TABLE IF EXISTS `work_flow_publish_history`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `work_flow_publish_history` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `create_time` datetime DEFAULT NULL,
-  `op_user_id` int(11) DEFAULT NULL,
-  `op_user_name` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
-  `workflow_id` int(11) DEFAULT NULL,
-  `workflow_name` varchar(50) DEFAULT NULL ,
-  `publish_state` tinyint(4) DEFAULT NULL ,
-  `type` tinyint(4) DEFAULT NULL COMMENT '' ,
-  `publish_reason` text CHARACTER SET utf8mb4 ,
-  `git_sha1` varchar(40) DEFAULT NULL,
-  `in_use` tinyint(1) NOT NULL DEFAULT '0',
-  `op_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT comment 'id',
+  `create_time` datetime DEFAULT NULL comment 'create_time',
+  `op_user_id` int(11) DEFAULT NULL comment 'op_user_id',
+  `op_user_name` varchar(50) CHARACTER SET utf8 DEFAULT NULL comment 'op_user_name',
+  `workflow_id` int(11) DEFAULT NULL comment 'workflow_id',
+  `workflow_name` varchar(50) DEFAULT NULL comment 'workflow_name' ,
+  `publish_state` tinyint(4) DEFAULT NULL comment 'publish_state',
+  `type` tinyint(4) DEFAULT NULL COMMENT 'type' ,
+  `publish_reason` text CHARACTER SET utf8mb4  comment 'publish_reason',
+  `git_sha1` varchar(40) DEFAULT NULL comment 'git_sha1',
+  `in_use` tinyint(1) NOT NULL DEFAULT '0' comment 'in_use',
+  `op_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment 'op_time',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
