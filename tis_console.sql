4cb1458bce1affd02430f12e6a6ff8eee64a7197
baisui <baisui@2dfire.com>
2019-04-22 10:00:24 +0800

    add comment

--- tis_console.sql
+++ tis_console.sql
@@ -139,7 +139,7 @@ DROP TABLE IF EXISTS `nums`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `nums` (
-  `a` int(11) NOT NULL commnet 'a'
+  `a` int(11) NOT NULL comment 'a'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
